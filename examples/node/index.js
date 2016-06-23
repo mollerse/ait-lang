1a1bb4b2acbc60f406d6363b7fa837862f24db85
Stian Veum Møllersen <stian.moellersen@gmail.com>
2016-06-23 21:36:01 +0200

    Remove examples -- moved to own repo

--- examples/node/index.js
+++
@@ -1,6 +0,0 @@
-const Node = require('../../src/runtime/runtimes/node');
-
-const runtime = Node();
-runtime.setCanvasDimensions(500, 500);
-runtime.evaluate(`${__dirname}/source.ait`);
-runtime.writeImage(`${__dirname}/test.png`);
